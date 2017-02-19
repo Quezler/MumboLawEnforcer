7ce70594bf4396bae447df2cfca554bf93d0aedd
Quezler <Quezler@me.com>
2017-02-19 17:02:03 +0100

    Implement ‘dig straight down’

---
+++ readme.md
@@ -0,0 +1,2 @@
+### dig straight down
+You cannot remove a stone block you are standing on in survival mode
--- src/net/queztech/mumbolawenforcer/Events.java
+++ src/net/queztech/mumbolawenforcer/Events.java
@@ -1,6 +1,10 @@
 package net.queztech.mumbolawenforcer;
 
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 class Events implements Listener {
 
@@ -8,5 +12,16 @@ class Events implements Listener {
 
     Events(MLE mle) {
         this.plugin = mle;
+    }
+
+    @EventHandler
+    public void didStraightDown(BlockBreakEvent event) {
+        if (event.getPlayer().getLocation().add(0, -1, 0).getBlock().equals(event.getBlock())) { // is targeting block below player
+            if (event.getBlock().getType().equals(Material.STONE)) {
+                if (event.getPlayer().getGameMode().equals(GameMode.SURVIVAL)) {
+                    event.setCancelled(true);
+                }
+            }
+        }
     }
 }
