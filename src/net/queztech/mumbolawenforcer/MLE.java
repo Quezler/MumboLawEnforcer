088f2fcf69281b86b12847a8812aca7be7b923a0
Quezler <Quezler@me.com>
2017-02-19 16:41:08 +0100

    Initialise plugin

---
+++ src/net/queztech/mumbolawenforcer/MLE.java
@@ -0,0 +1,12 @@
+package net.queztech.mumbolawenforcer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MLE extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Boes and hoes <3");
+    }
+
+}
